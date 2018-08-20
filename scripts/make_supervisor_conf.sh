550af7c6b6fb3dd1d22306910b5b651480f30043
leswing <lilleswing@gmail.com>
2018-08-20 10:18:09 -0400

    Fix deploy paths

--- scripts/make_supervisor_conf.sh
+++ scripts/make_supervisor_conf.sh
@@ -2,7 +2,7 @@
 export CUR_DIR=`pwd`
 export USER=`whoami`
 
-if [ -d /var/log/workserver ]; then
+if [ ! -d /var/log/workserver ]; then
     sudo mkdir /var/log/workserver
     sudo chown $USER:$USER /var/log/workserver
 fi
@@ -13,8 +13,8 @@ echo "directory=$CUR_DIR" >> scripts/workserver.conf
 echo "autostart=true" >> scripts/workserver.conf
 echo "autorestart=true" >> scripts/workserver.conf
 echo "startretries=3" >> scripts/workserver.conf
-echo "stderr_logfile=/var/log/kudos/workserver.err.log" >> scripts/workserver.conf
-echo "stdout_logfile=/var/log/kudos/workserver.out.log" >> scripts/workserver.conf
+echo "stderr_logfile=/var/log/workserver/workserver.err.log" >> scripts/workserver.conf
+echo "stdout_logfile=/var/log/workserver/workserver.out.log" >> scripts/workserver.conf
 echo "user=$USER" >> scripts/workserver.conf
 
 sudo cp scripts/workserver.conf /etc/supervisor/conf.d/workserver.conf
