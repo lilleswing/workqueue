86f515a7a85778204d961a3be869b3b035fc4975
leswing <lilleswing@gmail.com>
2018-08-20 10:04:54 -0400

    Check for file first

--- scripts/make_run_server.sh
+++ scripts/make_run_server.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 export PORT=8003
 
-rm scripts/run_server.sh
+if [ -f scripts/run_server.sh ] ; then
+    rm scripts/run_server.sh
+fi
 echo "#!/bin/bash" >> scripts/run_server.sh
 echo "export PATH=$PATH" >> scripts/run_server.sh
 echo "python manage.py migrate" >> scripts/run_server.sh
